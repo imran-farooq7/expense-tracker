9f495053fc34e076edab3b7f151d6491baf2f97c
imran farooq <imran_farooq7@hotmail.com>
2021-08-27 00:56:29 +0500

    Expense item component updated

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,12 @@
 function ExpenseItem() {
-    return <h2>expense item there</h2>;
+    return (
+			<div>
+				<div>March 28 2021</div>
+				<div>
+					<h2>Car insurance</h2>
+				</div>
+				<div>$294.67</div>
+			</div>
+		);
 }
 export default ExpenseItem;
