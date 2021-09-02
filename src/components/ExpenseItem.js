4161a6f5d62246fb41a730825fc4bbb8b260bcf6
imran farooq <imran_farooq7@hotmail.com>
2021-09-03 03:04:33 +0500

    updated expense item with dynamic values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import "./ExpenseItem.css"
+
 function ExpenseItem() {
+	const expenseDate = new Date(2021,2,28);
+	const expenseTitle = "Car insurance"
+	const expenseAmount = 294.67
     return (
 			<div className="expense-item">
-				<div>March 28 2021</div>
+				<div>{expenseDate.toISOString()}</div>
 				<div className="expense-item__description">
-					<h2>Car insurance</h2>
-					<div className="expense-item__price">$294.67</div>
+					<h2>{expenseTitle}</h2>
+					<div className="expense-item__price">${expenseAmount}</div>
 				</div>
 			</div>
 		);
