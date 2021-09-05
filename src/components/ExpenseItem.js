7e819b088340774255dfe29e73f93cf41511cc37
imran farooq <imran_farooq7@hotmail.com>
2021-09-06 01:24:16 +0500

    updated expense item with props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,15 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem() {
-	const expenseDate = new Date(2021,2,28);
-	const expenseTitle = "Car insurance"
-	const expenseAmount = 294.67
+function ExpenseItem(props) {
+	// const expenseDate = new Date(2021,2,28);
+	// const expenseTitle = "Car insurance"
+	// const expenseAmount = 294.67
     return (
 			<div className="expense-item">
-				<div>{expenseDate.toISOString()}</div>
+				<div>{props.date}</div>
 				<div className="expense-item__description">
-					<h2>{expenseTitle}</h2>
-					<div className="expense-item__price">${expenseAmount}</div>
+					<h2>{props.title}</h2>
+					<div className="expense-item__price">${props.amount}</div>
 				</div>
 			</div>
 		);
