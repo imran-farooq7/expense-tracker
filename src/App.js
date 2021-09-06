f52d97ee6605db401aba93b0c6e2ff334fe4939b
imran farooq <imran_farooq7@hotmail.com>
2021-09-07 03:52:15 +0500

    destructuring date props in expense item component

--- src/App.js
+++ src/App.js
@@ -27,21 +27,21 @@ function App() {
 		<div>
 			<h1>Hi there</h1>
 			<ExpenseItem
-				title={expenses[0].title}
+				title="Toilet Paper"
 				amount={expenses[0].amount}
-				date={expenses[0].date.toISOString()}></ExpenseItem>
+				date={expenses[0].date}></ExpenseItem>
 			<ExpenseItem
 				title={expenses[1].title}
 				amount={expenses[1].amount}
-				date={expenses[1].date.toISOString()}></ExpenseItem>
+				date={expenses[1].date}></ExpenseItem>
 			<ExpenseItem
 				title={expenses[2].title}
 				amount={expenses[2].amount}
-				date={expenses[2].date.toISOString()}></ExpenseItem>
+				date={expenses[2].date}></ExpenseItem>
 			<ExpenseItem
 				title={expenses[3].title}
 				amount={expenses[3].amount}
-				date={expenses[3].date.toISOString()}></ExpenseItem>
+				date={expenses[3].date}></ExpenseItem>
 		</div>
 	);
 }
