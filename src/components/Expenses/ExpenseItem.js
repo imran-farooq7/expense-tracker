c1082bdf048c52c87c790b0719afafce97ef3c65
imran farooq <imran_farooq7@hotmail.com>
2021-10-01 02:35:42 +0500

    How Component functions works

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,13 +2,19 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+	const clickHandler = () => {
+		title = "Updated";
+		console.log(title);
+	};
+	let title = props.title;
 	return (
 		<div className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
+			<button onClick={clickHandler}>Change title</button>
 		</div>
 	);
 };
